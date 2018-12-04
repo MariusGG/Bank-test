e2c40b5b7537d7c6becccdc0585355dc9fc5d8a3
mariusgg <marius.g.guei@gmail.com>
2018-12-04 14:40:33 +0000

    pass account log test

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class Account
-  attr_reader :balance
+  attr_reader :balance, :log
   def initialize
     @balance = 0
+    @log = []
   end
 
   def deposit(amount)
     raise 'Not an integer' unless amount.is_a? Numeric
 
     @balance += amount
+      @log.push(credit: amount, balance: @balance)
   end
 
   def withdraw(amount)
@@ -18,6 +20,8 @@ class Account
       raise 'Insufficient funds'
     else
       @balance -= amount
+      @log.push(debit: amount, balance: @balance)
     end
   end
+
 end
