03a4d4e6799a67642a9f6aeca805ff15fd785899
mariusgg <marius.g.guei@gmail.com>
2018-12-04 12:19:50 +0000

    refactored failing test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,13 +10,13 @@ describe Account do
   end
 
   describe '#deposit' do
-    it 'allows the client to deposit money' do
-      subject.deposit(100)
-      expect(subject.balance).to eq 100
-    end
-    it 'can only deposit integers' do
-     expect { subject.deposit('abc') }.to raise_error 'Not an integer'
-   end
+      it 'allows the client to deposit money' do
+        subject.deposit(100)
+        expect(subject.balance).to eq 100
+      end
+      it 'has to be an integer to deposit' do
+       expect { subject.deposit('abc') }.to raise_error('Not an integer')
+     end
   end
 
   describe '#withdraw' do
@@ -25,8 +25,8 @@ describe Account do
       subject.withdraw(50)
       expect(subject.balance).to eq 50
     end
-    it 'can only withdraw integers' do
-     expect { subject.withdraw('abc') }.to raise_error 'Not an integer'
+    it 'has to be an integer to withdraw' do
+     expect { subject.withdraw('abc') }.to raise_error('Not an integer')
    end
   end
 
