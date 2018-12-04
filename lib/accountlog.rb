1e7f02315f5ab444ed8566fc1c6a08d502f83776
mariusgg <marius.g.guei@gmail.com>
2018-12-04 22:56:15 +0000

    passing accountlog test

--- lib/accountlog.rb
+++ lib/accountlog.rb
@@ -2,4 +2,17 @@
 
 class Accountlog
 
+  attr_reader :log
+  def initialize
+    @log = []
+  end
+  
+  def deposit_log(amount, balance)
+    @log.push(credit: amount, balance: balance)
+  end
+
+  def withdraw_log(amount, balance)
+    @log.push(debit: amount, balance: balance )
+  end
+
 end
